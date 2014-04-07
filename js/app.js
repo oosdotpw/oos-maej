266a51f4c52470e763f65826c7188f2252c0cae3
oranzhang <423794590@qq.com>
2014-04-07 21:36:47 +0800

    new design ... sign progress

--- js/app.js
+++ js/app.js
@@ -6,3 +6,15 @@ App.Router.map(function() {
   this.route('account');
   this.route('settings');
 });
+App.DiscoveryRoute = Ember.Route.extend({
+  setupController: function(controller) {
+    controller.set('title', "Discover oos.pw!");
+  }
+})
+
+$(document).ready(function(){
+	$("#navi").toggle();
+	$(".app.title").click(function(){
+		$("#navi").toggle();
+	})
+});
